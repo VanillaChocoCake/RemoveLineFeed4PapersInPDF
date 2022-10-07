8d1cefe6a3678deb8e34804593cfb49eafe54129
孙培涵 <sunpeihan@mail.ustc.edu.cn>
2022-10-07 22:19:20 +0800

    fix

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 while True:
     whole = ""
+    print("New message:")
     while True:
         message = input()
         if message == "":
             break
         whole = f'{whole} {message}'
-    print(f'The whole message is :\n{whole}')
+    print(f'The whole message is :\n{whole}\n')
 
 
